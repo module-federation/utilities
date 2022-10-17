c89dcb921975c186310cd0ed7554e020f7de6282
Alex Vukov <47781709+alex-vukov@users.noreply.github.com>
2022-10-17 10:26:29 +0300

    Update index.ts

--- runtime/cssBoundary/src/index.ts
+++ runtime/cssBoundary/src/index.ts
@@ -1,43 +1,37 @@
 // Create a shadow container with all styles and a placeholder for the app injection
-exports.createShadowInstance = function (parentElementId: string): HTMLDivElement {
-  const { styles, instances } = window[__webpack_runtime_id__ + "styleContainer"];
+exports.createShadowInstance = function (parentElementId) {
+  const { styles, instances } = window['css-boundary-' + __webpack_runtime_id__];
   const shadowContainer = document.getElementById(parentElementId);
-  if (shadowContainer === null) {
-    throw new Error("Parent Id Not Found");
-  }
   // Block all styles coming from the light DOM
-  shadowContainer.style.all = "initial";
-  shadowContainer.attachShadow({ mode: "open", delegatesFocus: true });
-  if (!shadowContainer.shadowRoot) {
-    throw new Error("shadowRoot was not created successfully for parent " + parentElementId);
-  }
-  shadowContainer.shadowRoot.append(...styles.map((style) => style.cloneNode(true)));
+  shadowContainer.style.all = 'initial';
+  shadowContainer.attachShadow({ mode: 'open', delegatesFocus: true });
+  shadowContainer.shadowRoot.append(...styles.map(style => style.cloneNode(true)));
   // Create a body element so that reboot CSS rules work in the shadow DOM
-  const body = document.createElement("body");
+  const body = document.createElement('body');
   // Create a placeholder for the React app
-  const appPlaceholder = document.createElement("div");
-  appPlaceholder.id = "app-placeholder";
+  const appPlaceholder = document.createElement('div');
+  appPlaceholder.id = 'app-placeholder';
   body.appendChild(appPlaceholder);
   shadowContainer.shadowRoot.appendChild(body);
   instances[parentElementId] = shadowContainer;
   return appPlaceholder;
 };
 
-exports.deleteShadowInstance = function (id: string) {
-  const { instances } = window[__webpack_runtime_id__ + "styleContainer"];
+exports.deleteShadowInstance = function (id) {
+  const { instances } = window['css-boundary-' + __webpack_runtime_id__];
   delete instances[id];
 };
 
-exports.insert = function (style: HTMLElement) {
-  const sc = window[__webpack_runtime_id__ + "styleContainer"] || {};
-  if (typeof sc.isStandalone === "undefined") {
-    window[__webpack_runtime_id__ + "styleContainer"] = {
+exports.insert = function (style) {
+  const sc = window['css-boundary-' + __webpack_runtime_id__] || {};
+  if (typeof sc.isStandalone === 'undefined') {
+    window['css-boundary-' + __webpack_runtime_id__] = {
       styles: [],
       instances: {},
       isStandalone: false,
     };
   }
-  const { styles, instances, isStandalone } = window[__webpack_runtime_id__ + "styleContainer"];
+  const { styles, instances, isStandalone } = window['css-boundary-' + __webpack_runtime_id__];
   // Update the style list for newly created shadow instances
   styles.push(style);
 
@@ -47,7 +41,7 @@ exports.insert = function (style: HTMLElement) {
     // Update the style list for already existing shadow instances.
     // This will provide them with any lazy loaded styles.
     Promise.resolve().then(() => {
-      Object.values(instances).forEach((instance) => {
+      Object.values(instances).forEach(instance => {
         instance.shadowRoot.appendChild(style.cloneNode(true));
       });
     });
@@ -57,7 +51,7 @@ exports.insert = function (style: HTMLElement) {
 // If this function is called it will make the style loader behave as it normally does
 // and insert the styles into the head of the document instead of the shadow DOM
 exports.runStandalone = function () {
-  let { styles } = window[__webpack_runtime_id__ + "styleContainer"];
-  window[__webpack_runtime_id__ + "styleContainer"].isStandalone = true;
-  styles.forEach((style) => document.head.appendChild(style));
+  let { styles } = window['css-boundary-' + __webpack_runtime_id__];
+  window['css-boundary-' + __webpack_runtime_id__].isStandalone = true;
+  styles.forEach(style => document.head.appendChild(style));
 };
