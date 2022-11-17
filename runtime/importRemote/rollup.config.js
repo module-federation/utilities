39939605d5e8b9bd5ff8e4e7fdd244b5caf55d20
alex-vukov <alexvukov@gmail.com>
2022-11-17 10:32:12 +0200

    Remove Terser

--- runtime/importRemote/rollup.config.js
+++ runtime/importRemote/rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
 
 const extensions = [".js", ".ts", ".tsx", ".json"];
 
@@ -32,7 +31,6 @@ const config = {
       babelHelpers: "runtime",
       extensions,
     }),
-    terser(),
   ],
 };
 
